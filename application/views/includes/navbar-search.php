c29d18574180fa45d800e024d2b43daa3a74f9d8
Joyce Van Herck <joyce.vherck@gmail.com>
2013-06-07 13:56:41 +0200

    Style.css

--- application/views/includes/navbar-search.php
+++ application/views/includes/navbar-search.php
@@ -1,7 +1,7 @@
 <div class="navbar">
 	<div class="button-container clearfix">
 		<a href="<?php echo site_url('brainstormList') ?>" class="navbar-button" id="home-button"><span>home</span></a>
-		<a href="<?php echo site_url('???') ?>" class="navbar-button" id="profile-button"><span>profile</span></a>
+		<a href="<?php echo site_url('profile') ?>" class="navbar-button" id="profile-button"><span>profile</span></a>
 		<a href="<?php echo site_url('search') ?>" class="navbar-button active" id="search-button"><span>search</span></a>
 	</div>
 </div>
