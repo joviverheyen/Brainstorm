68dd5d11ea00aeb9667b342e6b933a2e0e02d318
Jovi Verheyen <jovi.verheyen@me.com>
2013-06-09 14:17:38 +0200

    Frontend add categories to a new brainstorm
    checkboxes, style and limit to max 3 selected checkboxes

--- application/views/addBrainstorm_view.php
+++ application/views/addBrainstorm_view.php
@@ -3,13 +3,25 @@
 	
 	<?php include('includes/titlebar.php')?>
 	
-	<div class="form">
+	<div class="form" name="brainstormForm">
 		<?php echo form_open('addBrainstorm/add'); ?>
 			<input type="text" name="add-brainstorm-title" id="add-brainstorm-title" placeholder="The title of your brainstorm"/> <br />
 			<textarea name="add-brainstorm-text" id="add-brainstorm-text" placeholder="Give some extra info about your idea..."></textarea> <br />
 			
+			
+			<div class="categories clearfix" name="categories">
+				<input type="checkbox" id="one" name="category" value="one"/>
+				<label for="one"><span class="category-button">one</span></label>
+				<input type="checkbox" id="two" name="category" value="two"/>
+				<label for="two"><span class="category-button">two</span></label>
+				<input type="checkbox" id="three" name="category" value="two"/>
+				<label for="three"><span class="category-button">three</span></label>
+				<input type="checkbox" id="four" name="category" value="two"/>
+				<label for="four"><span class="category-button">four</span></label>
+			</div>
+			
+			<br/>
 			<h3>TODO: </h3>
-			<p>category</p>
 			<p>images</p>
 			<p>private?</p>
 			<p>invite people</p>
@@ -19,6 +31,15 @@
 	</div>
 
 	<?php include('includes/navbar-home.php')?>
+	
+	<script type="text/javascript">
+		$('.categories :checkbox').change(function () {
+			var $cs=$(this).closest('.categories').find(':checkbox:checked');
+			if ($cs.length > 3) {
+				this.checked=false;
+			}
+		});
+	</script>
 
 </body>
 </html>
